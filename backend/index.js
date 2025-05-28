33c931352cf0c3adf3d31753cf5d462051416bc4
ROHIT SHARMA <102727841+rohitsharma716@users.noreply.github.com>
2025-05-29 01:37:24 +0530

    express version of graphql

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,43 @@
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
+
 import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import { expressMiddleware } from '@as-integrations/express5';
+
 import mergedTypeDefs from "./typeDefs/index.js";
 import mergedResolvers from "./resolvers/index.js";
+
+const app = express();
+// Our httpServer handles incoming requests to our Express app.
+// Below, we tell Apollo Server to "drain" this httpServer,
+// enabling our servers to shut down gracefully.
+const httpServer = http.createServer(app);
+
 const server = new ApolloServer({
      typeDefs: mergedTypeDefs,
      resolvers : mergedResolvers,
+     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 })
 
-const {url} = await startStandaloneServer(server)
+await server.start();
 
-console.log(`🚀  Server ready at: ${url}`);
+
+
+app.use(
+  '/',
+  cors(),
+  express.json(),
+  // expressMiddleware accepts the same arguments:
+  // an Apollo Server instance and optional configuration options
+  expressMiddleware(server, {
+    context: async ({ req }) => ({ req}),
+  }),
+);
+
+// Modified server startup
+await new Promise((resolve) =>
+  httpServer.listen({ port: 4000 }, resolve),
+);
+console.log(`🚀 Server ready at http://localhost:4000/`);
